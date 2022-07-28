555fc48acc19f332f68c128a9fa5f25300180040
Sahid <shasan1807013@gmail.com>
2022-07-28 18:19:48 +0600

    App Upload

---
+++ app/src/main/java/com/example/cardiacrecorder_013_015/activity/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.cardiacrecorder_013_015.activity;
+
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.cardiacrecorder_013_015.R;
+import com.example.cardiacrecorder_013_015.adapter.TaskAdapter;
+import com.example.cardiacrecorder_013_015.bottomSheetFragment.popUpScreenForAddingRecord;
+import com.example.cardiacrecorder_013_015.database.DatabaseClient;
+import com.example.cardiacrecorder_013_015.model.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends BaseActivity implements popUpScreenForAddingRecord.setRefreshListener {
+
+    @BindView(R.id.taskRecycler)
+    RecyclerView taskRecycler;
+    @BindView(R.id.addNewHealthRecord)
+    TextView addTask;
+    TaskAdapter taskAdapter;
+    List<Task> tasks = new ArrayList<>();
+    @BindView(R.id.noDataImage)
+    ImageView noDataImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setUpAdapter();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        PackageManager pm = getPackageManager();
+
+        //For initializing popup screen
+        addTask.setOnClickListener(view -> {
+          popUpScreenForAddingRecord popUpScreenForAddingRecord = new popUpScreenForAddingRecord();
+           popUpScreenForAddingRecord.setTaskId(0, false, this, MainActivity.this);
+           popUpScreenForAddingRecord.show(getSupportFragmentManager(), popUpScreenForAddingRecord.getTag());
+        });
+
+        getSavedTasks();
+    }
+
+    public void setUpAdapter() {
+        taskAdapter = new TaskAdapter(this, tasks, this);
+        taskRecycler.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        taskRecycler.setAdapter(taskAdapter);
+    }
+
+    private void getSavedTasks() {
+
+        class GetSavedTasks extends AsyncTask<Void, Void, List<Task>> {
+            @Override
+            protected List<Task> doInBackground(Void... voids) {
+                tasks = DatabaseClient
+                        .getInstance(getApplicationContext())
+                        .getAppDatabase()
+                        .dataBaseAction()
+                        .getAllTasksList();
+                return tasks;
+            }
+
+            @Override
+            protected void onPostExecute(List<Task> tasks) {
+                super.onPostExecute(tasks);
+                noDataImage.setVisibility(tasks.isEmpty() ? View.VISIBLE : View.GONE);
+                setUpAdapter();
+            }
+        }
+
+        GetSavedTasks savedTasks = new GetSavedTasks();
+        savedTasks.execute();
+    }
+
+    @Override
+    public void refresh() {
+        getSavedTasks();
+    }
+}
